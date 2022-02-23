7605ccc9497da6af6a20270f0d2f4f1c0635e3f6
JadeJ888 <garrettsjolly@gmail.com>
2022-02-22 20:32:05 -0600

    Added reload mechanics to gun script

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public Transform bulletSpawn;
     public Rigidbody bulletPrefab;
+   
+    [Range(10,100)]
+    public float bulletForce = 50;
 
     public bool debug = false; //set true to display debug msgs
+    //private variables
+    public int totalAmmo = 60;
+    public int clipSize = 10;
+    public int clip = 0;
+
+    public void Reload() {
+        if(clip == clipSize) {                              
+            if(debug) Debug.Log("Clip is already full.");
+            return;
+        }
+
+        int partialClip = 0;
+        if(clip > 0) partialClip = clip;    
+
+        if(totalAmmo + clip >= clipSize) {       
+            totalAmmo -= (clipSize - clip);       
+            clip = clipSize;             
+        } else {
+            // throw the rest of the ammo into the clip
+            clip = totalAmmo + clip;
+            totalAmmo = 0;
+        }  
+    }
 
     public void Fire() {
-        if(debug) Debug.Log("Pow!");
-        // create a copy of the bullet prefab
-        Rigidbody bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
-        // move the bullet in front of the gun
-        bullet.transform.Translate(0,0,1);
-        // add forward force to the gun
-        bullet.AddRelativeForce(Vector3.forward * 20, ForceMode.Impulse);
+        
+        if(clip > 0) {
+            if(debug) Debug.Log("Pow!");
+            clip -= 1;
+            // create a copy of the bullet prefab
+            Rigidbody bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+            // move the bullet in front of the gun
+            bullet.transform.Translate(0,0,1);
+            // add forward force to the gun
+            bullet.AddRelativeForce(Vector3.forward * bulletForce, ForceMode.Impulse);
+        } else {
+            if(debug) Debug.Log("Out of Ammo!");
+        }
     }
 }
